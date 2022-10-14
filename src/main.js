3c05edff9bb124127a7275dfabb990ae56fce6b2
Ngô Thuý Vy <ngothuyvy@MacBook-Air-cua-Ngo.local>
2022-10-14 09:35:23 +0700

    make it better

--- src/main.js
+++ src/main.js
@@ -4,16 +4,27 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const db = require("./config/db");
+const cors = require('cors')
 const route = require("./routes/route");
 
 db.connectDB();
+
+
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin','*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  req.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app
   .use(bodyParser.json())
   .use(morgan("combined"))
 
+
+
 route(app);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, cors(), () => {
   console.log("Example app listening on port");
 });
--- src/routes/hash.js
+++ src/routes/hash.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors')
 const express = require('express');
 const HashController = require('../app/controllers/HashController');
 const router = express.Router();
@@ -6,10 +7,10 @@ const router = express.Router();
 
 
 router
-    .post('/', HashController.index)
-    .get('/', HashController.get)
-    .get('/address', HashController.getItemUA)
-    .get('/test', HashController.test)
+    .post('/',cors() , HashController.index)
+    .get('/',cors(), HashController.get)
+    .get('/address',cors(), HashController.getItemUA)
+    .get('/test',cors(), HashController.test)
 
 
 
