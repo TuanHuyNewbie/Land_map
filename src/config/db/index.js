0fafbd32445ca65bec46a1c06dafcbe1ca94d717
Ngô Thuý Vy <ngothuyvy@MacBook-Air-cua-Ngo.local>
2022-10-14 10:17:17 +0700

    make it better

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
+
 URL = "mongodb+srv:tuanhuy:tuanhuy123@cluster0.s39zawz.mongodb.net/Test?retryWrites=true&w=majority"
 
 const connectDB = async () => {
