a31fdb58efd0172cd4c960600716c7c26827f3b8
Ngô Thuý Vy <ngothuyvy@MacBook-Air-cua-Ngo.local>
2022-10-14 10:34:52 +0700

    make it better

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-      await mongoose.connect(process.env.MONGODB_URI || URL, {
+      mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
